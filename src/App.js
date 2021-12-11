a2ad8d933fa27c1011b0d953d12fec215c3cae78
Sabareesh S <sabareesh1221@gmail.com>
2021-12-11 13:25:59 +0530

    Creating and styling CityComponent and WeatherInfoComponent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import styled from "styled-components";
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherInfoComponent";
 
 const Container =styled.div`
+  display: flex;
+  flex-direction:column;
+  margin: auto;
+  align-items: center;
+  box-shadow: 0 3px 6px 0 #555;
+  padding: 20px 10px;
+  border-radius: 4px;
+  width: 380px;
+  background: white;
+  font-family: 'Montserrat', sans-serif;
+`;
 
-`
+
+const AppLabel =styled.div`
+  color:black;
+  font-size: 18px;
+  font-weight: bold;
+`;
 
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <Container>
+      <AppLabel>Weather App</AppLabel>
+      <WeatherComponent />
+    </Container>
   );
 }
 
---
+++ src/modules/WeatherInfoComponent.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+const WeatherCondition = styled.div`
+display:flex;
+flex-direction: row;
+align-items: center;
+width:100%;
+justify-content:space-between;
+margin: 30px auto;
+`
+
+const Condition = styled.span`
+margin:20px auto;
+font-size: 14px;
+& span {
+    font-size: 28px;
+
+}
+`
+
+const WeatherLogo=styled.img`
+width: 100px;
+height: 100px;
+margin: 5px auto;
+`
+const Location=styled.span`
+font-size:28px;
+font-weight: bold;
+`
+
+const WeatherInfoLabel=styled.span`
+font-size:14px;
+font-weight: bold;
+margin: 20px 25px 10px;
+text-align:start;
+width: 90%;
+`
+
+const WeatherComponent =() => {
+    return (
+        <>
+        <WeatherCondition>
+            <Condition><span>30 C</span> | Cloudy</Condition>
+            <WeatherLogo src="/icons/perfect-day.svg"/>
+        </WeatherCondition>
+        <Location>London, GB</Location>
+        <WeatherInfoLabel>Weather Info</WeatherInfoLabel>
+        </>
+    )
+}
+export default WeatherComponent;
